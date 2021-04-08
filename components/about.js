55f6be808f2807647bf8744b3be91cee88e1da80
Josh <Joshuscurtis@gmail.com>
2021-04-08 13:29:23 +0100

    init

---
+++ components/about.js
@@ -0,0 +1,93 @@
+import React from "react";
+import styles from "../styles/About.module.css";
+//import "animate.js";
+import { OpeningTimes, News, Insta } from "./box";
+import Typewriter from "typewriter-effect";
+
+import ScrollAnimation from "react-animate-on-scroll";
+
+export default function Pane() {
+  const news = "Opening soon for takeaway drinks and cakes... ";
+  return (
+    <div id="about">
+      <div className={styles.People__Title__Container}>
+        {/* <h1 className="People__Title">ABOUT US</h1> */}
+      </div>
+      <div className={styles.About__Main}>
+        <div className={styles.About__row2}>
+          <div className={styles.exc}>
+            <Typewriter
+              options={{ loop: true }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Our coffee is ")
+                  .typeString(
+                    "<strong><span style='color: #00204e;'>top quality</span></strong>"
+                  )
+                  .pauseFor(500)
+                  .deleteChars(11)
+                  .typeString(
+                    "<strong><span style='color: #00204e;'>award winning</span></strong>"
+                  )
+                  .pauseFor(500)
+                  .deleteChars(13)
+                  .typeString(
+                    "<strong><span style='color: #00204e;'>hand roasted</span></strong>"
+                  )
+                  .pauseFor(500)
+                  .deleteChars(12)
+                  .typeString(
+                    "<strong><span style='color: #00204e;'>Fairtrade</span></strong>"
+                  )
+                  .pauseFor(500)
+                  .callFunction(() => {
+                    console.log(
+                      "<strong><span style='color: #00204e;'>String typed out!"
+                    );
+                  })
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .callFunction(() => {
+                    console.log("All strings were deleted");
+                  })
+                  .start();
+              }}
+            />
+          </div>
+        </div>
+        <div className={styles.About__row2}>
+          <ScrollAnimation
+            animateOnce={true}
+            duration={1}
+            animateIn="slideInLeft"
+          >
+            <OpeningTimes
+              title="Opening Times"
+              mon="8:30am - 3:00pm"
+              tue="8:30am - 3:00pm"
+              wed="8:30am - 3:00pm"
+              thu="8:30am - 3:00pm"
+              fri="8:30am - 3:00pm"
+              sat="8:30am - 3:00pm"
+              sun="CLOSED"
+            />
+          </ScrollAnimation>
+          <ScrollAnimation
+            animateOnce={true}
+            duration={1}
+            animateIn="slideInRight"
+          >
+            <News url="https://christchurchdunstable.org.uk/wp-json/wp/v2/posts?include[]=470&include[]=7912" title="Latest Updates" content={news} />
+          </ScrollAnimation>
+        </div>
+
+          <div className={styles.About__row2}>
+          <ScrollAnimation animateOnce={true} animateIn="fadeIn">
+            <Insta title="" />
+          </ScrollAnimation>
+        </div>
+   
+      </div>
+    </div>
+  );
+}
---
+++ components/hero.js
@@ -0,0 +1,37 @@
+import styles from '../styles/Home.module.css'
+import Button from "@material-ui/core/Button";
+import Carousel from "nuka-carousel";
+
+export default function Hero() {
+    return (
+<div className={styles.Panel__Main}>
+<Carousel autoplay={true} autoplayInterval={5000} wrapAround={true}>
+  {/*<div className="bg_1"></div>*/}
+  <div className={styles.bg_2}></div>
+  <div className={styles.bg_3}></div>
+  <div className={styles.bg_4}></div>
+</Carousel>
+<div className={styles.Panel__Box}>
+  <img
+    alt="The Way Logo"
+    src="https://www.christchurchdunstable.org.uk/theway/images/min/logo-min.jpg"
+    className={styles.logo}
+  ></img>
+  <div className={styles.Main__Buttons}>
+    <Button
+      size="large"
+      className={styles.Menu__Button}
+      variant="contained"
+      href="/menu"
+    >
+      Our Menu
+  </Button>
+    <Button size="large" variant="contained">
+      Our Events
+  </Button>
+    <Button size="large" variant="contained">
+      About Us
+  </Button>
+  </div>
+</div>
+</div>)}
