0fea2abc799e5e833738985cb27672cb0d4fa9b4
Josh <Joshuscurtis@gmail.com>
2021-04-09 00:50:57 +0100

    api

--- api/wp.py
+++ api/wp.py
@@ -1,15 +1,18 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
-
+import requests
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
 
+
+    r = requests.get('https://christchurchdunstable.org.uk/wp-json/wp/v2/posts?include[]=470&include[]=8913')
+
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str("Hey").encode())
+    self.wfile.write(str(r.json()).encode())
     return
 
 
