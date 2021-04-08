f8234998a9c211036617bf79e0a8d1f000e80ff5
Josh <Joshuscurtis@gmail.com>
2021-04-09 00:59:29 +0100

    api

--- api/wp.py
+++ api/wp.py
@@ -10,7 +10,7 @@ class handler(BaseHTTPRequestHandler):
     r = requests.get('https://christchurchdunstable.org.uk/wp-json/wp/v2/posts?include[]=470&include[]=8913')
 
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header('Content-type', 'application/json')
     self.end_headers()
     self.wfile.write(str(r.json()).encode())
     return
