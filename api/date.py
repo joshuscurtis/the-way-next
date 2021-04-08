0ba269bfac9571e68e3956b226ba4228a81d4531
Josh <Joshuscurtis@gmail.com>
2021-04-08 14:19:16 +0100

    none

--- api/date.py
+++ api/date.py
@@ -1,14 +1,10 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
-from igramscraper.instagram import Instagram
 
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    instagram = Instagram()
-    account = instagram.get_account_by_id(1478189341)
-
 
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
