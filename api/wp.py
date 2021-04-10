13388adbbf42d31c5c586ab2e4e1f1ddc4eebabb
Josh <Joshuscurtis@gmail.com>
2021-04-10 13:05:58 +0100

    api

--- api/wp.py
+++ api/wp.py
@@ -2,18 +2,25 @@ from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 import requests
 
+
 class handler(BaseHTTPRequestHandler):
 
-  def do_GET(self):
+    def do_GET(self):
 
+        r = requests.get(
+            'https://christchurchdunstable.org.uk/wp-json/wp/v2/posts?include[]=470&include[]=8913')
+        a = requests.get(
+            'https://christchurchdunstable.org.uk/wp-json/wp/v2/posts?include[]=470&include[]=8913')
+        b = requests.get(
+            'https://christchurchdunstable.org.uk/wp-json/wp/v2/posts?include[]=470&include[]=8913')
+        c = requests.get(
+            'https://christchurchdunstable.org.uk/wp-json/wp/v2/posts?include[]=470&include[]=8913')
+        d = requests.get(
+            'https://christchurchdunstable.org.uk/wp-json/wp/v2/posts?include[]=470&include[]=8913')
 
-    r = requests.get('https://christchurchdunstable.org.uk/wp-json/wp/v2/posts?include[]=470&include[]=8913')
-
-    self.send_response(200)
-    self.send_header('Content-type', 'application/json')
-    self.end_headers()
-    self.wfile.write(str(r.json()[0]['content']['rendered']).encode())
-    return
-
-
-    
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(str(r.json()[0]['content']['rendered']+a.json()[0]['content']['rendered']+b.json()[
+                         0]['content']['rendered']+c.json()[0]['content']['rendered']+d.json()[0]['content']['rendered']).encode())
+        return
